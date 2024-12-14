558e89dc25ffcba33c5d551912eb3b4373b8ac9e
Jaren Garman <jarengarman@protonmail.com>
2024-12-14 16:10:19 -0500

    feature: Add earnPermit function

--- dmv/exercises/registrant.js
+++ dmv/exercises/registrant.js
@@ -1,10 +1,8 @@
-
-
 class createRegistrant {
   constructor(nameParameter, ageParameter, permitParameter = false) {
-    this.name = nameParameter
-    this.age = ageParameter
-    this.permit = permitParameter
+    this.name = nameParameter;
+    this.age = ageParameter;
+    this.permit = permitParameter;
     this.licenseData = {
       written: false,
       license: false,
@@ -12,8 +10,13 @@ class createRegistrant {
     };
   }
 }
-module.exports = {
-  createRegistrant
-  // earnPermit,
-  // takeLicenseExam
+
+function earnPermit(registrantParameter) {
+  registrantParameter.permit = true;
+  return "You have successfully earned a permit!";
 }
+module.exports = {
+  createRegistrant,
+  earnPermit,
+  // takeLicenseExam
+};
