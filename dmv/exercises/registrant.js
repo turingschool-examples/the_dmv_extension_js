2340e2c36fada247c4ab13d2940853ce5c4cff29
Sebastian McKee <sebmcke@gmail.com>
2024-12-17 11:47:38 -0600

    Add earmPermit and takeLicenseExam methods

--- dmv/exercises/registrant.js
+++ dmv/exercises/registrant.js
@@ -12,7 +12,21 @@ module.exports = {
         renewed: false
       }
     }
+  },
+
+  earnPermit(registrant) {
+    if (registrant.age >= 16) {
+      registrant.permit = true;
+      return "You have successfully earned a permit!";
+    }
+    return "Sorry, you are not able to earn a permit right now.";
+  },
+
+  takeLicenseExam(registrant) {
+    if (registrant.permit) {
+      registrant.license = true;
+      return "Great job! You can take your test.";
+    }
+    return "Sorry, you need to earn a permit first."
   }
-  // earnPermit,
-  // takeLicenseExam
 }
