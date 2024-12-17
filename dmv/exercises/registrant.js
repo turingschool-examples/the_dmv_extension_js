910b390a4e8ff4408ca4b7080781338d96c35621
Sebastian McKee <sebmcke@gmail.com>
2024-12-17 11:41:14 -0600

    Add registrant initialization

--- dmv/exercises/registrant.js
+++ dmv/exercises/registrant.js
@@ -1,7 +1,18 @@
 
 
 module.exports = {
-  // createRegistrant,
+  createRegistrant(name, age, permit=false) {
+    return {
+      name: name,
+      age: age,
+      permit: permit,
+      licenseData: {
+        written: false,
+        license: false,
+        renewed: false
+      }
+    }
+  }
   // earnPermit,
   // takeLicenseExam
 }
